fd0740b7c11e5fb31e60a77ebff457d1019958ef
James Malcolm <jcallinicos@gmail.com>
2018-01-21 22:59:32 +1300

    Adding final project

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Enron Fraud Udacity Machine Learning Final Project
 
-This repo and markdown is all about identifying Persons of Interest in the Enron scandal of 2001. This project relates to Udacity's Capstone Project for the <a href="https://www.udacity.com/course/intro-to-machine-learning--ud120">ntroduction to Machine Learning Course</a>.
+This repo and markdown is all about identifying Persons of Interest in the Enron scandal of 2001. This project relates to Udacity's Capstone Project for the <a href="https://www.udacity.com/course/intro-to-machine-learning--ud120">Introduction to Machine Learning Course</a>.
 
 # Pre-requistes/Requirements:
 
@@ -35,10 +35,57 @@ I also removed a row of the dataset relating to THE TRAVEL AGENCY IN THE PARK, a
 
 On top of the given features, I created two new features. These aim to represent a relationship between the number of emails sent/received to/from a POI vs the total number of emails sent/received overall. It's represented by the following:
 
-$$ \frac{fromThisPersonToPOI}{fromMessages} $$
-$$ \frac{toThisPersonFromPOI}{toMessages} $$
+![img](http://latex.codecogs.com/svg.latex?%5Cfrac%7BfromThisPersonToPOI%7D%7BfromMessages%7D)
 
-Further processing on this data is simply using the MinMaxScaler from the sklearn package 
+![img](http://latex.codecogs.com/svg.latex?%5Cfrac%7BtoThisPersonFromPOI%7D%7BtoMessages%7D)
+
+Further processing on this data is done simply using the MinMaxScaler from the sklearn package. Scaling is generally needed for many classifiers as metric used for some classifiers (such as K Neighbours) is based on the distance between points so we need to normalise the data points for accuracy and model performance.
+
+### The Models
+
+For this project, I selected three classification models. These were:
+* Adaboost
+* K-Neighbours
+* Decision Tree Classification.
+
+These models were initally run without any parameter hyper-tuning, then the parameters were tuned. In all cases, the model performed better with parameter tuning.
+
+### The Best Model
+
+In a Fraud Detection Application such as this, I would favour a model with a slightly higher False Positive rate (that is, Model predicts Positive, but condition is Negative). Reasoning behind this, is that if a model was used almost as part of exploratory analysis, it can be helpful to identify a pool of potential POIs for further investigation rather than missing potential POIs with a strict model. 
+
+For the purpose of this investigation, I solely used the F1 Score to judge the models.
+
+<table>
+  <tr>
+    <th>Model</th>
+    <th>Precision</th>
+    <th>Recall</th>
+    <th>F1 Score</th>
+  </tr>
+  <tr>
+    <td>AdaBoost</td>
+    <td>0.79</td>
+    <td>0.80</td>
+    <td>0.79</td>
+  </tr>
+  <tr>
+    <td>K-Neighbours</td>
+    <td>0.84</td>
+    <td>0.80</td>
+    <td>0.75</td>
+  </tr>
+  <tr>
+    <td>Decision Tree</td>
+    <td>0.89</td>
+    <td>0.87</td>
+    <td>0.85</td>
+  </tr>
+</table>
+
+Parameters used for the Decision Tree were:
+{'feature_selection__k': 7, 'model__splitter': 'random', 'model__criterion': 'gini', 'model__random_state': 42}
+
 # References
 
 ### Articles
--- poi_id.py
+++ poi_id.py
@@ -2,24 +2,62 @@
 
 import sys
 import pickle
-sys.path.append("../tools/")
+sys.path.append("./tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 
+import matplotlib.pyplot as plt
+from sklearn.feature_selection import SelectKBest
+import sklearn.pipeline
+from sklearn.model_selection import GridSearchCV
+
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = ['poi', 'salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus',
+                 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 
+                 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock',
+                 'director_fees', 'to_messages', 'email_address', 'from_poi_to_this_person', 
+                 'from_messages', 'from_this_person_to_poi', 'shared_receipt_with_poi']
+  
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
+
 ### Task 2: Remove outliers
+features = ["salary", "bonus"]
+data_dict.pop('TOTAL', 0)
+data_dict.pop('THE TRAVEL AGENCY IN THE PARK', 0)
+data = featureFormat(data_dict, features)
+
 ### Task 3: Create new feature(s)
+
+for key in data_dict:
+    # Create from_ratio. Set from_ratio to zero if NaN appears
+    if (data_dict[key]['from_this_person_to_poi'] == 'NaN') or (data_dict[key]['from_messages'] == 'NaN'):
+        data_dict[key]['from_ratio'] = 0
+    else:
+        data_dict[key]['from_ratio'] = (1.0*data_dict[key]['from_this_person_to_poi']/data_dict[key]['from_messages'])
+    # Create to_ratio. set to_ratio to zero if NaN appears
+    if (data_dict[key]['from_poi_to_this_person']=='NaN') or (data_dict[key]['to_messages']=='NaN'):
+          data_dict[key]['to_ratio'] = 0
+    else:  
+        data_dict[key]['to_ratio'] = (1.0*data_dict[key]['from_poi_to_this_person']/data_dict[key]['to_messages']) 
+		
+features_list = ["poi", "salary", "bonus", 'deferral_payments', 'total_payments', 'loan_advances', 
+                 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses',
+                 'exercised_stock_options', 'long_term_incentive', 'shared_receipt_with_poi', 'restricted_stock', 
+                 'director_fees', 'from_ratio', 'to_ratio'] # add new features to dataset
+data = featureFormat(data_dict, features_list)
+	
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
+data = featureFormat(my_dataset, features_list)
+
+
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
@@ -32,8 +70,24 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+from sklearn.tree import DecisionTreeClassifier
+
+select_best_dt = SelectKBest(k=10)
+clf_dt = DecisionTreeClassifier()
+
+steps = [('feature_selection', select_best_dt),
+        ('model', clf_dt)]
+
+pipeline_dt = sklearn.pipeline.Pipeline(steps)
+
+parameters = dict(feature_selection__k = [3, 5, 7, 9, 11, 12, 15],
+                  model__criterion = ['entropy', 'gini'],
+                  model__random_state = [42],
+                  model__splitter = ['best', 'random'])
+
+grid_search = GridSearchCV(pipeline_dt, param_grid=parameters)
+grid_search.fit(features,labels)
+
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -46,6 +100,10 @@ clf = GaussianNB()
 from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
+
+clf = grid_search.best_estimator_
+clf.fit(features_train, labels_train)
+predictions = clf.predict(features_test)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
